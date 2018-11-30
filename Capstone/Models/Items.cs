ab40dfb453dd84216e9b9150a7fb8107aaa66eb6
Santa <iskrewitup@gmail.com>
2018-11-30 14:08:07 +1300

    Updated Items DB

--- Capstone/Models/Items.cs
+++ Capstone/Models/Items.cs
@@ -8,11 +8,12 @@ namespace Capstone.Models
     public class Items
     {
         public int ID { get; set; }
-        public string Name { get; set; }
-        public int AmmountInKgs { get; set; }
-        public string Supplier { get; set; }
-        public DateTime UseBy { get; set; }
-        public int LowerThreshhold { get; set; }
+        public string Name { get; set; }  //filter by "Item"
+        public int Quantity{ get; set; }  //filter by "Quantity Remaining"
+        public string QuantityType { get; set; } //filter by "Quantity Type" eg: kg, mg, number
+        public int BaseQuantity { get; set; }  //filter by "Base Quantity"
+        public DateTime CreationDate { get; set; }
+        public DateTime UseBy { get; set; } //filter by "Expiration Date"
         public bool IsDeleted { get; set; }
     }
 }
