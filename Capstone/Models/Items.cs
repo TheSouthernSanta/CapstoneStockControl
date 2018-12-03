9afc9428f98ac18fb7c1ff34baff8b1693c98a8a
Santa <iskrewitup@gmail.com>
2018-12-03 13:18:50 +1300

    Added in UI Changes as well as Item Class Changes

--- Capstone/Models/Items.cs
+++ Capstone/Models/Items.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -11,8 +12,10 @@ namespace Capstone.Models
         public string Name { get; set; }  //filter by "Item"
         public int Quantity{ get; set; }  //filter by "Quantity Remaining"
         public string QuantityType { get; set; } //filter by "Quantity Type" eg: kg, mg, number
+        [Display(Name = "Base Quantity")]
         public int BaseQuantity { get; set; }  //filter by "Base Quantity"
         public DateTime CreationDate { get; set; }
+        [Display(Name = "Use By")]
         public DateTime UseBy { get; set; } //filter by "Expiration Date"
         public bool IsDeleted { get; set; }
     }
